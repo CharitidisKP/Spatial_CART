d4df8a580f31e96fbf8802a30ce9635a8ffcd239
Konstantinos Panagiotis Charitidis <charitidisk@gmail.com>
2025-11-06 01:32:27 +0100

    Added function to shift dataframe columns and edited main script to adjust for the changes

--- Scripts/My_Functions.R
+++ Scripts/My_Functions.R
@@ -1,3 +1,43 @@
+## Function to shift and fix dataframes ##
+Correct_Dataframes <- function(df) {
+  ## Only works on dataframes ##
+  stopifnot(is.data.frame(df))
+  
+  ## Get the total number of columns so we know the last's position
+  n <- ncol(df)
+  ## Make sure there are columns
+  if (n == 0) return(df)
+  
+  ## Copy to work on ##
+  out <- df
+  
+  ## Shift columns to the left (col i becomes original col i + 1) ##
+  if (n >= 2) out[1:(n-1)] <- df[2:n]
+  
+  ## Make last column NAs ##
+  out[[n]] <- NA
+  
+  ## Split the second to last at the comma ##
+  ## Left value stays at n - 1 while right value at n ##
+  pen <- out[[n-1]]
+  has_comma <- !is.na(pen) & grepl(",", pen, fixed = TRUE)
+  
+  ## Extract left and right values using the final comma ##
+  left_vals  <- ifelse(has_comma, sub(",[^,]*$", "", pen), pen)
+  right_vals <- ifelse(has_comma, sub("^.*,", "", pen), NA)
+  
+  ## Assign to the proper columns and remove whitespaces ##
+  out[[n-1]] <- trimws(left_vals)
+  out[[n]] <- trimws(right_vals)
+  
+  ## Type convert the values so numeric stays numeric and chr stays chr ##
+  out[] <- lapply(out, function(x) type.convert(x, as.is = TRUE))
+  
+  ## Return the fixed dataframe ##
+  out
+}
+
+
 ## Function to load RDS files ##
 Load_My_Rds <- function(dir, file_pattern = "(?i)\\.rds$", slide_regex = "\\.Slide(\\d+)\\.") {
   
