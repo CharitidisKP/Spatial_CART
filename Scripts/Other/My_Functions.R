1c411400a50824b288a1be2b30a4245586187abe
Konstantinos Panagiotis Charitidis <charitidisk@gmail.com>
2025-11-25 15:06:04 +0100

    Added functions for the cell annotation analysis

--- Scripts/Other/My_Functions.R
+++ Scripts/Other/My_Functions.R
@@ -140,3 +140,91 @@ lbl_cells_percent <- function(b) {
   paste0(format(round(b * ncol(Counts_filtered)), 
                 big.mark = ","), " (", scales::percent(b, accuracy = 1), ")")
 }
+
+
+
+## Function to visualise cell annotation results ##
+plot_flightpath_noleg <- function(flightpath_obj,
+                            annotation_obj,
+                            prefix = "Flightpath_") {
+  
+  ## Get object name simplify for the title ##
+  obj_name <- deparse(substitute(flightpath_obj))
+  dataset_tag <- sub(paste0("^", prefix), "", obj_name) ## Remove "Flightpath_" ##
+  current_ref_dataset <- gsub("_", " ", dataset_tag) ## Replace underscores for within the plot ##
+  
+  ## Plot the cells ##
+  Celltypes_df <- flightpath_obj$cellpos %>% 
+    as.data.frame() %>% 
+    mutate(Cluster = factor(Annotation_6k_KRCC$clust))
+  
+  ## Cluster label positions ##
+  Celltypes_pos_df <- flightpath_obj$clustpos %>% 
+    as.data.frame() %>% 
+    rownames_to_column(var = "Cluster") %>% 
+    mutate(Cluster = factor(Cluster))
+  
+  ## Scatter plot ##
+  ggplot(Celltypes_df, aes(x = x, y = y, color = Cluster)) +
+    geom_point(size = 0.2, alpha = 0.8) +
+    geom_text(data = Celltypes_pos_df, aes(x = x, y = y, label = Cluster),
+              color = "black", size = 3, show.legend = FALSE) +
+    coord_equal() +
+    theme_void() +
+    theme(legend.position = "none", 
+          plot.margin = margin(0, 0, 0, 0)) +
+    labs(title = paste0("Annotation results using the: ", 
+                        current_ref_dataset, 
+                        " reference dataset"))
+}
+
+## Visualisation 2, with the legend ##
+
+plot_flightpath_leg <- function(flightpath_obj,
+                            annotation_obj,
+                            prefix = "Flightpath_") {
+  
+  ## Get object name simplify for the title ##
+  obj_name <- deparse(substitute(flightpath_obj))
+  dataset_tag <- sub(paste0("^", prefix), "", obj_name) ## Remove "Flightpath_" ##
+  current_ref_dataset <- gsub("_", " ", dataset_tag) ## Replace underscores for within the plot ##
+  
+  Cluster_stats <- tibble(Cluster = Flightpath_6k_KRCC$clust) %>%
+    group_by(Cluster) %>%
+    summarise(n_cells = n(), .groups = "drop") %>%
+    mutate(mean_conf = Flightpath_6k_KRCC$meanconfidence, 
+           Cluster_lab = sprintf("%s (n = %s, mean conf = %.2f)", Cluster, 
+                                 scales::comma(n_cells), mean_conf), 
+           Cluster = factor(Cluster)) %>% 
+    arrange(desc(n_cells))
+  
+  ## Plot the cells ##
+  Celltypes_df <- flightpath_obj$cellpos %>% 
+    as.data.frame() %>% 
+    mutate(Cluster = factor(Annotation_6k_KRCC$clust)) %>% 
+    left_join(Cluster_stats, by = "Cluster") %>%
+    mutate(Cluster_lab = factor(Cluster_lab, levels = Cluster_stats$Cluster_lab))
+  
+  ## Cluster label positions ##
+  Celltypes_pos_df <- flightpath_obj$clustpos %>% 
+    as.data.frame() %>% 
+    rownames_to_column(var = "Cluster") %>% 
+    mutate(Cluster = factor(Cluster)) %>% 
+    left_join(Cluster_stats, by = "Cluster") %>%
+    mutate(Cluster_lab = factor(Cluster_lab, levels = Cluster_stats$Cluster_lab))
+  
+  ## Scatter plot ##
+  ggplot(Celltypes_df, aes(x = x, y = y, colour = Cluster_lab)) +
+    geom_point(size = 0.2, alpha = 0.8) +
+    geom_text(data = Celltypes_pos_df, aes(x = x, y = y, label = Cluster),
+              colour = "black", size = 3, show.legend = FALSE) +
+    coord_equal() +
+    theme_void() +
+    labs(title = paste0("Annotation results using the: ", 
+                        current_ref_dataset, 
+                        " reference dataset"), 
+         colour = "InSituType Clusters") +
+    guides(colour = guide_legend(override.aes = list(size = 4), ncol = 1)) +
+    theme(legend.position = "right", 
+          plot.margin = margin(0, 0, 0, 0)) 
+}
